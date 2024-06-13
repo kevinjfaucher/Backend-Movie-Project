a40adf902203d3001816a684e286e90e86ea5fb7
kevinjfaucher <126988757+kevinjfaucher@users.noreply.github.com>
2024-06-12 23:39:04 -0400

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,14 @@
 const path = require("path");
 require("dotenv").config();
 const { parse } = require('pg-connection-string');
- 
+
 // Environment setup
 const {
   NODE_ENV = "development",
   DEVELOPMENT_DATABASE_URL,
   PRODUCTION_DATABASE_URL,
 } = process.env;
- 
+
 // Parse connection string and add SSL configuration for production
 const connectionConfig = parse(NODE_ENV === "production" ? PRODUCTION_DATABASE_URL : DEVELOPMENT_DATABASE_URL);
 if (NODE_ENV === "production") {
@@ -16,7 +16,7 @@ if (NODE_ENV === "production") {
     rejectUnauthorized: false // Be cautious with this in production environments
   };
 }
- 
+
 module.exports = {
   development: {
     client: "postgresql",
@@ -26,10 +26,10 @@ module.exports = {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
-    directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: path.join(__dirname, "src", "db", "seeds"),
     },
   },
- 
+
   production: {
     client: "postgresql",
     connection: connectionConfig,
@@ -38,10 +38,10 @@ module.exports = {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "dr", "seeds"),
+      directory: path.join(__dirname, "src", "db", "seeds"), // Corrected typo here
     },
   },
- 
+
   test: {
     client: "sqlite3",
     connection: {
